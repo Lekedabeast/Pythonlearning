ac62622232ead08c7706a34ced9ebd7b7ade9678
Lekedabeast <165559500+Lekedabeast@users.noreply.github.com>
2024-04-01 13:54:33 +1100

    Update ExerciseConvertingunits.py
    Fixingg  the upper command on the if statmemt line 6

--- ExerciseConvertingunits.py
+++ ExerciseConvertingunits.py
@@ -3,8 +3,8 @@ unit = input("What weight did you select? (K)g or (L)bs: ")
 K = (Weight * 0.4536)
 L =  (Weight * 2.205)
 
-if unit.upper == "K":
+if unit.upper() == "K":
     print('Weight in pounds: ',   L)
-elif unit.upper == "L":
+elif unit.upper() == "L":
     print('Weight in Kilograms: ',  K)
 print("Conversion Complete")
